cd8f0039388dfc672466a0411a4fa012457a0dc5
smog <abdielorellana3@gmail.com>
2021-08-16 20:11:52 -0400

    first commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './styles/styles.scss'
+export const JournalApp = () => {
+    return (
+        <div>
+            <h1>Jornal App</h1>
+            <hr />
+        </div>
+    )
+}
