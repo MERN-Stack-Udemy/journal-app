4b3840a186095d1a1766f4e5b2beaeea2647e9dc
smog <abdielorellana3@gmail.com>
2021-08-17 21:43:03 -0400

    Add styles for NoteScreen component

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -6,6 +6,29 @@ export const NoteScreen = () => {
         <div className ="notes__main-contend">
             
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    type="text" 
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea 
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                >
+
+                </textarea>
+
+                <div className="notes__image">
+                    <img 
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqF2wRRfu6aPbs6YjofxfPmcEWQI-A4Ig6zA&usqp=CAU" 
+                        alt="some image" 
+                    />
+                </div>
+            </div>
         </div>
     )
 }
