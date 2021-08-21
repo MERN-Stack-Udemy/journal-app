ba3f0b1095fbc35fe98f60a824d178958b9e62d2
smog <abdielorellana3@gmail.com>
2021-08-21 11:52:09 -0400

    implement firebase google auth login in LoginScreen

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDgd4DLBSDYhswI9Jvk3irNBROwMyw1rP8",
@@ -10,11 +10,11 @@ const firebaseConfig = {
     messagingSenderId: "57146579995",
     appId: "1:57146579995:web:858d5ee8af2b5f509e6cbe"
   };
-  // Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
-const googleAuthProvider = firebase.auth.GoogleAuthProvider();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export {
     db,
