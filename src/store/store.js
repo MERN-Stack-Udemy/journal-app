5cdfc18980f267230d5bb82fcc41f8d44fca8e07
smog <abdielorellana3@gmail.com>
2021-08-21 16:31:50 -0400

    implements uiReducer

--- src/store/store.js
+++ src/store/store.js
@@ -6,14 +6,16 @@ import {
 import thunk from 'redux-thunk';
 
 import { authReducer } from '../reducers/authReducer';
+import { uiReducer } from '../reducers/uiReducer';
 
 const composeEnhancers = (
         typeof window !== 'undefined' && 
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
-    auth: authReducer
     // add your reducers here
+    auth: authReducer,
+    ui: uiReducer
 });
 
 export const store = createStore(
--- src/types/types.js
+++ src/types/types.js
@@ -1,5 +1,9 @@
 
 export const types = {
+    // types for authReducer
     login : '[Auth] Login',
     logout : '[Auth] Logout',
+    // types for uiReducer
+    uiSetError: '[UI] Set Error',
+    uiRemoveError: '[UI] Remove Error',
 }
