3d4663284fa2c006089a3e825121964b4e070d8b
smog <abdielorellana3@gmail.com>
2021-08-23 00:05:53 -0400

    implement logout action in JournalScreen logout button

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -13,14 +13,27 @@ import { login } from '../actions/auth';
 
 export const AppRouter = () => {
 
+    const [checking, setChecking] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     const dispatch = useDispatch();
+    
     useEffect(() => {
         firebase.auth().onAuthStateChanged( (user) => {
             if(user?.uid){
                 dispatch( login( user.uid, user.displayName ));
+                setIsLoggedIn(true);
+            }else {
+                setIsLoggedIn(false);
             }
+            setChecking(false);
         })
-    }, [ dispatch ]);
+    }, [ dispatch, setChecking, setIsLoggedIn ]);
+
+    if(checking){
+        return (
+            <h1> Loading.... </h1>
+        )
+    }
 
     return (
         <Router>
