eb19a2bc91e743200a14e11213b992738a9878a3
smog <abdielorellana3@gmail.com>
2021-08-23 21:13:21 -0400

    implements loadNotes reducer method and save into storage/notes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,6 +11,7 @@ import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import { PrivateRoutes } from './PrivateRoutes';
 import { PublicRoute } from './PublicRoute';
+import { startLoadingNotes } from '../actions/notes';
 
 export const AppRouter = () => {
 
@@ -19,10 +20,11 @@ export const AppRouter = () => {
     const dispatch = useDispatch();
     
     useEffect(() => {
-        firebase.auth().onAuthStateChanged( (user) => {
+        firebase.auth().onAuthStateChanged( async (user) => {
             if(user?.uid){
                 dispatch( login( user.uid, user.displayName ));
                 setIsLoggedIn(true);
+                dispatch( startLoadingNotes( user.uid ) )
             }else {
                 setIsLoggedIn(false);
             }
